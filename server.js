2b08e94540f00b4024bf352e436bc171ad5ec9bf
Usr2Zays <ct3803778@gmail.com>
2026-07-30 01:35:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const express = require('express');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const Database = require('better-sqlite3');
+const { DatabaseSync } = require('node:sqlite');
 
 const PORT = process.env.PORT || 3000;
 
